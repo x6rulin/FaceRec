43bf8b0089a3f46c4e6851aab218256234ea62d8
rulin <x6rulin@163.com>
2019-08-08 23:05:39 +0800

    cifar10

---
+++ cifar10.py
@@ -0,0 +1,29 @@
+import os
+import sys
+import torchvision
+
+rootPath = os.path.abspath(os.path.dirname(__file__))
+sys.path.append(rootPath)
+
+from core.model import MainNet
+from core.train import Train
+
+if __name__ == "__main__":
+    data_dir = "data/cifar10"
+    if not os.path.exists(data_dir):
+        os.makedirs(data_dir, 0o775)
+
+    train_dataset = torchvision.datasets.CIFAR10(data_dir, train=True, download=False,
+                                             transform=torchvision.transforms.Compose([
+                                                 torchvision.transforms.RandomHorizontalFlip(),
+                                                 torchvision.transforms.ToTensor(),
+                                             ]))
+    val_dataset = torchvision.datasets.CIFAR10(data_dir, train=False, download=False,
+                                               transform=torchvision.transforms.Compose([
+                                                   torchvision.transforms.ToTensor(),
+                                               ]))
+    cls_num, feat_num = 10, 2
+
+    net = MainNet(10, 2)
+    trainer = Train(train_dataset, val_dataset, cls_num, feat_num, net)
+    trainer.main()
