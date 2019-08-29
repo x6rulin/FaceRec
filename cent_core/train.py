660e4e2d19f5cd92bd84ae3ec7217bd992744fa2
x6rulin <x6rulin@163.com>
2019-08-29 19:45:06 +0800

    scatter

--- cent_core/train.py
+++ cent_core/train.py
@@ -16,6 +16,36 @@ class Args(ArgParse):
         self.parser.add_argument("--scatter-dir", type=str, default="scatters", help="directory for feature-scatters pictures saving")
 
 
+class Scatter:
+
+    def __init__(self, cls_num):
+        self.__cls_num = cls_num
+        self.__colors = [f'C{i}' for i in range(cls_num)]
+        self.reset()
+
+    def reset(self):
+        self.__features = []
+        self.__labels = []
+
+    def update(self, features, labels):
+        self.__features.append(features.cpu().data)
+        self.__labels.append(labels.cpu().data)
+
+    def plot(self, _dir, prefix, _n):
+        features = torch.cat(self.__features).numpy()
+        labels = torch.cat(self.__labels).numpy()
+        for label_idx in range(self.__cls_num):
+            plt.scatter(features[labels == label_idx, 0], features[labels == label_idx, 1], c=self.__colors[label_idx], s=1)
+        plt.legend(['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck'], loc='upper right')
+
+        dirname = os.path.join(_dir, prefix)
+        if not os.path.exists(dirname):
+            os.makedirs(dirname, 0o775)
+        save_name = os.path.join(dirname, 'epoch_' + str(_n) + '.png')
+        plt.savefig(save_name, bbox_inches='tight')
+        plt.close()
+
+
 class Train(Trainer):
 
     def __init__(self, train_dataset, val_dataset, model, cls_num, feat_num):
@@ -33,10 +63,12 @@ class Train(Trainer):
         self.criterion = torch.nn.CrossEntropyLoss()
         self.value = 0.
 
+        self.scatter = Scatter(cls_num)
+
     def train(self):
         self.net.train()
+        self.scatter.reset()
         print(f"epochs: {self.epoch}")
-        all_features, all_labels = [], []
         for i, (data, labels) in enumerate(self.train_loader, start=1):
             data, labels = data.to(self.device), labels.to(self.device)
             features, outputs = self.net(data)
@@ -52,20 +84,17 @@ class Train(Trainer):
                     param.grad.data *= self.args.alpha / (self.args.beta * self.args.lr)
             self.optimizer.step()
 
-            all_features.append(features.cpu().data)
-            all_labels.append(labels.cpu().data)
+            self.scatter.update(features, labels)
 
             if i % self.args.print_freq == 0:
                 print(f"[epoch: {self.epoch} - {i}/{len(self.train_loader)}]"
                       f"Loss: {loss.float()} - Loss_xent: {xent_loss.float()} - Loss_cent: {cent_loss.float()}")
 
-        all_features = torch.cat(all_features).numpy()
-        all_labels = torch.cat(all_labels).numpy()
-        self.plot_features(all_features, all_labels, prefix='train')
+        self.scatter.plot(self.args.scatter_dir, 'train', self.epoch)
 
     def validate(self):
         self.net.eval()
-        all_features, all_labels = [], []
+        self.scatter.reset()
         correct, valid, total = 0, 0, 0
         for data, labels in self.val_loader:
             data, labels = data.to(self.device), labels.to(self.device)
@@ -77,31 +106,11 @@ class Train(Trainer):
             correct += (torch.argmax(outputs, dim=1) == labels.data).sum().float()
             valid += (predictions[:, 1] == labels.data[predictions[:, 0]]).sum().float()
 
-            all_features.append(features.cpu().data)
-            all_labels.append(labels.cpu().data)
+            self.scatter.update(features, labels)
 
-        all_features = torch.cat(all_features).numpy()
-        all_labels = torch.cat(all_labels).numpy()
-        self.plot_features(all_features, all_labels, prefix='val')
+        self.scatter.plot(self.args.scatter_dir, 'val', self.epoch)
 
         acc = 100 * correct / total
         val = 100 * valid / total
         print(f"[epochs: {self.epoch}]Valid: {val.float():.2f}% - Accuracy: {acc.float():.2f}%")
         return val.item()
-
-    def plot_features(self, features, labels, prefix):
-        colors = ['C0', 'C1', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9']
-        for label_idx in range(self.cls_num):
-            plt.scatter(
-                features[labels == label_idx, 0],
-                features[labels == label_idx, 1],
-                c=colors[label_idx],
-                s=1,
-            )
-        plt.legend(['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck'], loc='upper right')
-        dirname = os.path.join(self.args.scatter_dir, prefix)
-        if not os.path.exists(dirname):
-            os.makedirs(dirname, 0o775)
-        save_name = os.path.join(dirname, 'epoch_' + str(self.epoch) + '.png')
-        plt.savefig(save_name, bbox_inches='tight')
-        plt.close()
